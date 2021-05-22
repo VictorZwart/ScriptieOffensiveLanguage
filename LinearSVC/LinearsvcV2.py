33625cfe441dce1dbbcc33498d5401767949886a
VictorZwart <49715623+VictorZwart@users.noreply.github.com>
2021-05-22 09:48:39 +0200

    Updated LinearSVCV2
    However this one has become obsolete, there is a new and better version called LinearSVCV3

--- LinearSVC/LinearsvcV2.py
+++ LinearSVC/LinearsvcV2.py
@@ -15,48 +15,129 @@ from sklearn.metrics import confusion_matrix
 from sklearn import metrics
 
 
-def read_file(csv_file, goal):
-    df = pd.read_csv(csv_file, sep='\t')
-    df.head()
+# def read_file(csv_file, goal):
+#     df = pd.read_csv(csv_file, sep='\t')
+#     df.head()
+#
+#     col = [goal, 'text']
+#     df = df[col]
+#     df = df[pd.notnull(df['text'])]
+#     df.columns = [goal, 'text']
+#     df = df.replace(np.nan, 'None', regex=True)
+#     # df['offense'] = df['explicitness'] + ' ' + df['target']
+#     new_col = goal + '_id'
+#     df[new_col] = df[goal].factorize()[0]
+#     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1')
+#     features = tfidf.fit_transform(df.text).toarray()
+#     print(features.shape)
+#     print(df.head(10))
+#     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1')
+#     features = tfidf.fit_transform(df.text).toarray()
+#     print(features.shape)
+#     return features, df
 
-    col = [goal, 'text']
-    df = df[col]
-    df = df[pd.notnull(df['text'])]
-    df.columns = [goal, 'text']
-    df = df.replace(np.nan, 'None', regex=True)
-    # df['offense'] = df['explicitness'] + ' ' + df['target']
-    new_col = goal + '_id'
-    df[new_col] = df[goal].factorize()[0]
+def preprocess(line):
+    # first lowercase:
+    line = line.lower()
+
+    #remove mentions:
+    line = re.sub(r'(@\w+)','MENTION', line)
+
+    #remove url's:
+    line = re.sub(r'(https\S+)','URL',line)
+
+    #remove all numbers
+    line  = re.sub(r'[0-9]+', 'NUMBER', line)
+
+    #remove all hashtags
+    line  = re.sub(r'#', '', line)
+
+
+    return line
+
+
+def train_data(csv_file):
+    full_data = []
+    with open(csv_file, encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for index, line in enumerate(reader):
+            text = preprocess(line['text'])
+            full_data.append([text, line['explicitness'], line["target"]])
+    df_full = pd.DataFrame(full_data)
+    df_full.columns = ["text", "explicitness", "target"]
+    # print(len(df_full))
+    df_full = df_full[df_full.explicitness != '']
+    df_full['explicitness_id'] = df_full['explicitness'].factorize()[0]
+    df_full = df_full.dropna(subset=['explicitness'])
+    # print(len(df_full))
+    print(df_full.head(10))
     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1')
-    features = tfidf.fit_transform(df.text).toarray()
-    print(features.shape)
-    print(df.head(10))
+    features = tfidf.fit_transform(df_full.text).toarray()
+
+    return features, df_full
+
+def vectorize_data(data, vocab): # First vectorize the data
+    keys = list(vocab.keys())
+    filter_unknown = lambda word: vocab.get(word, None) is not None
+    encode = lambda review: list(map(keys.index, filter(filter_unknown, review)))
+    vectorized = list(map(encode, data))
+    return vectorized
+
+def test_data(csv_file):
+    full_data = []
+    with open(csv_file, encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for index, line in enumerate(reader):
+            text = preprocess(line['text'])
+            full_data.append([text, line['abusive'], line["target"]])
+
+    df_full = pd.DataFrame(full_data)
+    df_full.columns = ["text", "explicitness", "target"]
+    print(len(df_full))
+    df_full = df_full[df_full.explicitness != '']
+    df_full['explicitness_id'] = df_full['explicitness'].factorize()[0]
+    df_full = df_full.dropna(subset=['explicitness'])
+    print(len(df_full))
+    print(df_full.head(10))
     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1')
-    features = tfidf.fit_transform(df.text).toarray()
-    print(features.shape)
-    return features, df
+    features = tfidf.fit_transform(df_full.text).toarray()
+
+    return features, df_full
 
 
-def train(features, labels, df, goal):
+
+def train(x_train, x_test, y_train, y_test, df):
     model = LinearSVC()
-    X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index,
-                                                                                     test_size=0.2, random_state=0)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
     conf_mat = confusion_matrix(y_test, y_pred)
     print(conf_mat)
-    print(metrics.classification_report(y_test, y_pred, target_names=df[goal].unique()))
+    # print(df[goal].unique())
+    print(metrics.classification_report(y_test, y_pred, target_names=df['explicitness'].unique()))
 
 
-def main(csv_file):
-    ex_features, ex_df = read_file(csv_file, 'explicitness')
-    ex_labels = ex_df.explicitness_id
-    tar_features, tar_df = read_file(csv_file, 'target')
-    tar_labels = tar_df.target_id
+def main(files):
+    train_file, test_file = files
+    x_train, train_df = train_data(train_file)
 
-    train(ex_features, ex_labels, ex_df, 'explicitness')
-    train(tar_features, tar_labels, tar_df, 'target')
+    y_train = train_df.explicitness_id
+    x_test, test_df = test_data(test_file)
+    y_test = test_df.explicitness_id
+    # tar_features, tar_df = read_file(csv_file, 'target')
+    # tar_labels = tar_df.target_id
+
+    print(x_train)
+    print(x_test)
+    print(y_train)
+    print(y_test)
+    x_train_text = train_df["text"]
+    x_test_text = test_df["text"]
+    y_test_text = test_df["explicitness"]
+    y_train_text = train_df["explicitness"]
+
+    train(x_train,x_test, y_train, y_test, test_df)
+    # train(tar_features, tar_labels, tar_df, 'target')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1:])
